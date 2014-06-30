fd94d2eb22c4a0b9ceeba3d98c1e8db46b654d7a
hoonanwoo <hoonanwoo@wooHoo.org>
2014-06-30 15:13:43 -0400

    add password generator updated with user defined length

---
+++ gPasswordGen.py
@@ -0,0 +1,22 @@
+import random, string, io, os, sys
+   
+    def getCode( char = string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation):
+        try:
+            passWordLength = int(input("Define The Password Length With An Integer Value: "))
+            crtPass = "".join(random.choice(char) for x in range(passWordLength))
+         
+            """open file for password logging"""
+            fileStore_password = open('fileStore_passwordLOG','a')
+            fileStore_password.writelines(crtPass + '\n' )
+      
+            print("Generated Password: " ,crtPass)
+      
+         except TypeError:
+             print("Incorrect parameter: exception_TypeError")
+      
+""" 
+This is a Alphanumeric Password Genarator
+it is default setting is for 10 characters
+you can enter the any other length you desire
+"""
+
