cde3cb3003078680d6a00b397a1bea8c218830b9
hoonanwoo <hoonanwoo@wooHoo.org>
2014-06-30 21:39:30 -0400

    spell checking. That is all.

--- gPasswordGen.py
+++ gPasswordGen.py
@@ -1,22 +1,27 @@
-import random, string, io, os, sys
+import random 
+import string 
+import io 
+import os
+import sys
    
-    def getCode( char = string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation):
-        try:
-            passWordLength = int(input("Define The Password Length With An Integer Value: "))
-            crtPass = "".join(random.choice(char) for x in range(passWordLength))
+def getCode( char = string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation):
+    try:
+        passWordLength = int(input("Define The Password Length With An Integer Value: "))
+        crtPass = "".join(random.choice(char) for x in range(passWordLength))
          
-            """open file for password logging"""
-            fileStore_password = open('fileStore_passwordLOG','a')
-            fileStore_password.writelines(crtPass + '\n' )
+        """open file for password logging"""
+        fileStore_password = open('fileStore_passwordLOG','a')
+        fileStore_password.writelines(crtPass + '\n' )
       
-            print("Generated Password: " ,crtPass)
+        print("Generated Password: " ,crtPass)
       
-         except TypeError:
-             print("Incorrect parameter: exception_TypeError")
-      
-""" 
+    except TypeError:
+        print("Incorrect parameter: exception_TypeError")
+
+
+"""
 This is a Alphanumeric Password Genarator
 it is default setting is for 10 characters
 you can enter the any other length you desire
 """
-
+      
