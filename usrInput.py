b14104d78fa2535449587095ad6b39ca3ecde148
devlp <devlp@devlp-Thnkx61.(none)>
2014-07-02 08:02:29 -0700

    Added a welcome msg and set a time delay for the usrs input

--- usrInput.py
+++ usrInput.py
@@ -1,4 +1,9 @@
 import gPasswordGen
+import time
 
+print ('''Welcome,
+          We hope this tool will help you keep your files and or accounts
+          more secure by generating better passwords''' + '\n') 
+time.sleep(1)
 print (gPasswordGen.getCode())
 
