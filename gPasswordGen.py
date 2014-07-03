58b2832bf46b7ee603d0c4ee1b7d61da7926dbf5
hoonanwoo <hoonanwoo@gmail.com>
2014-07-03 06:12:50 -0700

    Update gPasswordGen.py

--- gPasswordGen.py
+++ gPasswordGen.py
@@ -20,8 +20,10 @@ def getCode( char = string.ascii_uppercase + string.digits + string.ascii_lowerc
 
 
 """
-This is a Alphanumeric Password Genarator
-it is default setting is for 10 characters
-you can enter the any other length you desire.
+This is a Alphanumeric Password Genarator.
+The default behavioral setting is for password length of 10 characters.
+This default behavior is both overridable and user definable.
+You can enter the any other length you desire.
 """
       
+ 
