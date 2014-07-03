4204227ebb9733ae52b460640c97069c01a9c5e7
devlp <devlp@devlp-Thnkx61.(none)>
2014-07-03 13:27:39 -0700

    splck

---
+++ usrInput.py
@@ -0,0 +1,12 @@
+import gPasswordGen
+import time
+import os
+import sys
+
+print ('''Welcome,
+          We hope this tool will help you keep your files and or accounts
+          more secure by generating better passwords''' + '\n') 
+time.sleep(1)
+print (gPasswordGen.getCode())
+
+
