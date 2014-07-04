c9311fd0520ee5d9c8d180fb2347f6d16361551a
devlp <devlp@devlp-Thnkx61.(none)>
2014-07-04 00:12:06 -0700

    splck

--- gPasswordGen.py
+++ gPasswordGen.py
@@ -10,14 +10,15 @@ def getCode( char = string.ascii_uppercase + string.digits + string.ascii_lowerc
         crtPass = "".join(random.choice(char) for x in range(passWordLength))
          
         """open file for password logging"""
-        fileStore_password = open('fileStore_passwordLOG','a')
+        fileStore_password = open('fileStore_passwordLOGT','a')
         fileStore_password.writelines(crtPass + '\n' )
       
         print("Generated Password: " ,crtPass)
       
     except TypeError:
         print("Incorrect parameter: exception_TypeError")
-
+    except NameError:
+        print('Incorrect parameter: Enter a Number')
 
 """
 This is a Alphanumeric Password Genarator.
@@ -28,7 +29,7 @@ You can enter the any other length you desire.
 
 def main():
 	getCode()
-    
+
 if __name__ == '__main__':
 	main()      
 
